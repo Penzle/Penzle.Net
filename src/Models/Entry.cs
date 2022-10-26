d7224b5debf6c302ab5e77f10987bbfb46492262
Admir <admir.mujkic@penzle.com>
2022-10-26 07:48:27 +0200

    Entry.cs has had its limitations eliminated.

--- src/Models/Entry.cs
+++ src/Models/Entry.cs
@@ -1,6 +1,6 @@
 ﻿namespace Penzle.Core.Models;
 
-public abstract class Entry<TEntity> : BaseModel<EntrySystem> where TEntity : new()
+public class Entry<TEntity> : BaseModel<EntrySystem> where TEntity : new()
 {
     public TEntity Fields { get; set; } = new();
 }
