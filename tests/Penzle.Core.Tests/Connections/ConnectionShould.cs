e97cd6d642534a821b7fe29199b6702fd0282828
Admir <admir.mujkic@penzle.com>
2022-11-12 17:12:53 +0100

    Added more unit tests

--- tests/Penzle.Core.Tests/Connections/ConnectionShould.cs
+++ tests/Penzle.Core.Tests/Connections/ConnectionShould.cs
@@ -225,4 +225,117 @@ public class ConnectionShould
         response.System.ModifiedAt.Should().Be(new DateTime(year: 2022, month: 11, day: 08, hour: 07, minute: 53, second: 09, kind: DateTimeKind.Utc));
         response.System.CreatedAt.Should().Be(new DateTime(year: 2022, month: 10, day: 25, hour: 08, minute: 48, second: 10, kind: DateTimeKind.Utc));
     }
+
+    [Theory]
+    [ConnectionDependenciesData]
+    public async Task Ability_To_Send_Non_Entry_Generic_Get_Http_Request_And_Receive_Not_Found_Or_Null(
+        Uri baseAddress,
+        ApiOptions apiOptions,
+        ICredentialStore<BearerCredentials> credentialStore,
+        IHttpClient httpClientAdapter,
+        IJsonSerializer jsonSerializer,
+        IPlatformInformation platformInformation)
+    {
+        // Arrange
+        var mockHttp = new Mock<HttpMessageHandler>();
+        mockHttp
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound, Content = new StringContent(string.Empty)
+            });
+
+        httpClientAdapter = new HttpClientAdapter(() => mockHttp.Object);
+        var connection = new Connection(baseAddress: baseAddress, apiOptions: apiOptions, credentialStore: credentialStore, httpClient: httpClientAdapter, serializer: jsonSerializer, platformInformation: platformInformation);
+
+        // Act
+        var response = await connection.Get<Article>(new Uri(uriString: "/api/project/main/environment/development/entries/4d40413f-587c-442b-949e-c44771096ee3", uriKind: UriKind.Relative), new Dictionary<string, string>(), "text/json", "text/json", CancellationToken.None);
+
+        // Assert
+        response.Should().BeNull();
+    }
+
+    [Theory]
+    [ConnectionDependenciesData]
+    public async Task Ability_To_Send_Generic_Get_Http_Request_With_System_Properties(
+        Uri baseAddress,
+        ApiOptions apiOptions,
+        ICredentialStore<BearerCredentials> credentialStore,
+        IHttpClient httpClientAdapter,
+        IJsonSerializer jsonSerializer,
+        IPlatformInformation platformInformation)
+    {
+        // Arrange
+        var payload = await File.ReadAllTextAsync(path: "Fixtures/Payloads/GetContentEntry.json").ConfigureAwait(false);
+
+        var mockHttp = new Mock<HttpMessageHandler>();
+        mockHttp
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK, Content = new StringContent(payload)
+            });
+
+        httpClientAdapter = new HttpClientAdapter(() => mockHttp.Object);
+        var connection = new Connection(baseAddress: baseAddress, apiOptions: apiOptions, credentialStore: credentialStore, httpClient: httpClientAdapter, serializer: jsonSerializer, platformInformation: platformInformation);
+
+        // Act
+        var response = await connection.Get<ArticleWithSystem>(new Uri(uriString: "/api/project/main/environment/development/entries/4d40413f-587c-442b-949e-c44771096ee3", uriKind: UriKind.Relative), new Dictionary<string, string>(), "text/json", "text/json", CancellationToken.None);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Should().BeAssignableTo<ArticleWithSystem>();
+        response.Should().NotBeNull();
+        response.System.Should().NotBeNull();
+        response.System.Should().BeAssignableTo<BaseSystem>();
+
+        response.Banner.Should().Be("e879281a-a225-4f82-a4af-740884dcee1b");
+        response.Title.Should().Be("Getting started with Penzle");
+        response.Slug.Should().Be("getting-started-with-penzle");
+        response.Description.Should().Be("With the help of Penzle's headless content platform, developers can use the toolkits and languages most appropriate for the project.");
+        response.Body.Should().NotBeNullOrWhiteSpace();
+        response.RelatedArticle.Should().Be("c442e7ef-155e-4c28-8fe6-c094d07946ea");
+
+        response.System.Id.Should().Be("4d40413f-587c-442b-949e-c44771096ee3");
+        response.System.Template.Should().BeLowerCased(nameof(Article));
+        response.System.ParentId.Should().Be("16ea7830-fc9c-42f8-9b19-fe9d9c3d31bd");
+        response.System.Name.Should().Be("Getting started with Penzle");
+        response.System.Version.Should().Be("24.0");
+        response.System.Language.Should().Be("en-US");
+        response.System.Slug.Should().Be("getting-started-with-penzle");
+        response.System.ModifiedAt.Should().Be(new DateTime(year: 2022, month: 11, day: 08, hour: 07, minute: 53, second: 09, kind: DateTimeKind.Utc));
+        response.System.CreatedAt.Should().Be(new DateTime(year: 2022, month: 10, day: 25, hour: 08, minute: 48, second: 10, kind: DateTimeKind.Utc));
+    }
+
+    [Theory]
+    [ConnectionDependenciesData]
+    public async Task Ability_To_Send_Non_Entry_Generic_Get_Http_Request_And_Receive_Not_Content_Or_Null(
+        Uri baseAddress,
+        ApiOptions apiOptions,
+        ICredentialStore<BearerCredentials> credentialStore,
+        IHttpClient httpClientAdapter,
+        IJsonSerializer jsonSerializer,
+        IPlatformInformation platformInformation)
+    {
+        // Arrange
+        var mockHttp = new Mock<HttpMessageHandler>();
+        mockHttp
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NoContent, Content = new StringContent(string.Empty)
+            });
+
+        httpClientAdapter = new HttpClientAdapter(() => mockHttp.Object);
+        var connection = new Connection(baseAddress: baseAddress, apiOptions: apiOptions, credentialStore: credentialStore, httpClient: httpClientAdapter, serializer: jsonSerializer, platformInformation: platformInformation);
+
+        // Act
+        var response = await connection.Get<Article>(new Uri(uriString: "/api/project/main/environment/development/entries/4d40413f-587c-442b-949e-c44771096ee3", uriKind: UriKind.Relative), new Dictionary<string, string>(), "text/json", "text/json", CancellationToken.None);
+
+        // Assert
+        response.Should().BeNull();
+    }
 }
