84854085c946898cf63cf8cb72a1d3ed73e5cd58
Haris Hercegovac <haris.hercegovac@applicita.com>
2023-10-11 11:13:10 +0200

    Added include filter

---
+++ src/Models/Filters/IncludeFilter.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2022 Penzle LLC. All Rights Reserved. Licensed under the MIT license. See License.txt in the project root for license information.
+namespace Penzle.Core.Models.Filters
+{
+    public class IncludeFilter : IQueryParameter
+    {
+        private readonly int _depthLevel;
+        public IncludeFilter(int depthLevel)
+        {
+            _depthLevel = depthLevel;
+        }
+
+        /// <summary>
+        /// Returns the preview query parameter as a string.
+        /// </summary>
+        /// <returns>The encapsulated query parameter.</returns>
+        public string GetParameter() => $"include={_depthLevel}";
+    }
+}
